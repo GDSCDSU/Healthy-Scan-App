1e3dcadcfc5e29fd778843e673220c22c8d0e0dd
Bisharat56 <engineer.bisharatali@gmail.com>
2024-01-23 23:19:58 +0500

    sig up activity UI done

--- app/src/main/java/com/healthyscan/Login.java
+++ app/src/main/java/com/healthyscan/Login.java
@@ -2,13 +2,29 @@ package com.healthyscan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+//    TextView not_acc_sign_up_navigate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+//        not_acc_sign_up_navigate = findViewById(R.id.not_acc_sign_up_navigate);
+//        not_acc_sign_up_navigate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent navigate_signup_activity = new Intent(Login.this, Signup.class);
+//                startActivity(navigate_signup_activity);
+//            }
+//        });
+//
+   }
 }
