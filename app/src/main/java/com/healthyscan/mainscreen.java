843f9db404b74091c455682c6ce4fb458a4a59a7
Amna Tariq <amna90745@gmail.com>
2024-02-10 13:44:07 +0500

    I have created the sliders of the app

---
+++ app/src/main/java/com/healthyscan/mainscreen.java
@@ -0,0 +1,14 @@
+package com.healthyscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+    }
+}
